93d2dbd0580fef16d5b9f0e00f0bf5b2b9cffdae
Eduardo Lôbo Moreira <eduardolobo1717@yahoo.com>
2025-05-20 21:07:18 -0300

    New commit
    Added listing command and now executes files

--- Program.cs
+++ Program.cs
@@ -9,14 +9,17 @@ Console.OutputEncoding = System.Text.Encoding.UTF8;
 includes.Add("type");
 includes.Add("echo");
 includes.Add("exit");
+includes.Add("cd");
+includes.Add("ls");
+includes.Add("exec");
 
 while (true)
 {
     var tdr = Directory.GetCurrentDirectory();
-    Console.Write($"{tdr}>>> ");
+    Console.Write($"{tdr} → ");
     var command = Console.ReadLine();
 
-    if (command.StartsWith("echo ") == true)
+    if (command.StartsWith("echo "))
     {
         var echo = command.Substring(5).Trim();
         Console.WriteLine(echo);
@@ -28,10 +31,10 @@ while (true)
         break;
     }
 
-    if (command.StartsWith("type ") == true)
+    if (command.StartsWith("type "))
     {
         var echo = command.Substring(5).Trim();
-        if (includes.Contains(echo) == true)
+        if (includes.Contains(echo))
         {
             Console.WriteLine($"{echo}: is a command");
         }
@@ -42,11 +45,85 @@ while (true)
         continue;
     }
 
-    if (command.StartsWith("cd ") == true)
+    if (command.StartsWith("cd "))
     {
         var echo = command.Substring(3).Trim();
-        Directory.SetCurrentDirectory(echo); 
+        try
+        {
+            Directory.SetCurrentDirectory(echo);
+        }
+        catch (Exception error)
+        {
+            Console.WriteLine($"{error.Message}");
+        }
         continue;
+    }
+
+    if (command.StartsWith("ls"))
+    {
+        try
+        {
+            var list = Directory.EnumerateFileSystemEntries(tdr);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        catch (Exception error)
+        {
+            Console.WriteLine($"{error.Message}");
+        }
+        continue;
+    }
+
+    if (command.StartsWith("ls "))
+    {
+        var echo = command.Substring(3).Trim();
+        try
+        {
+            var list = Directory.EnumerateFileSystemEntries(echo);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        catch (Exception error)
+        {
+            Console.WriteLine($"{error.Message}");
+        }
+        continue;
+    }
+
+    if (command.StartsWith("exec "))
+    {
+        var echo = command.Substring(5).Trim();
+        var split = echo.Split(' ', 2, StringSplitOptions.RemoveEmptyEntries);
+        var program = split[0];
+        var parameter = split[1];
+
+        echo = Path.Combine(tdr, program);
+        if (File.Exists(echo))
+        {
+            var process = new System.Diagnostics.Process();
+            process.StartInfo.FileName = echo;
+            process.StartInfo.Arguments = parameter;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.WorkingDirectory = tdr;
+            process.StartInfo.RedirectStandardOutput = true;    
+            process.StartInfo.RedirectStandardError = true;
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            string error = process.StandardError.ReadToEnd();
+            process.WaitForExit();
+            Console.WriteLine(output);
+            Console.WriteLine(error);
+        }
+        else
+        {
+            Console.WriteLine($"{echo}: isn't a executable");
+        }
+
     }
 
     Console.WriteLine($"{command}: isn't a command");
